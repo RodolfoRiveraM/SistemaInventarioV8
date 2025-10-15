653e8604aba6b2475f88d1afb7258b40278ce825
rodolfo <rodolform987@gmail.com>
2025-10-14 21:10:08 -0600

    CRUD de bodega

--- SistemaInventario.AccesoDatos/Configuracion/BodegaConfiguracion.cs
+++ SistemaInventario.AccesoDatos/Configuracion/BodegaConfiguracion.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SistemaInventario.AccesoDatos.Data.Migrations.Configuracion
+namespace SistemaInventario.AccesoDatos.Configuracion
 {
     public  class BodegaConfiguracion : IEntityTypeConfiguration<Bodega>
     {
